12d0131d372cbc07474db33c7e5f19c42f7955f8
marcosVidou <mvidou@udesa.edu.ar>
2025-03-31 11:33:09 -0300

    primeros pasos, headers, controllers, cambios en app.js

---
+++ controllers/productsControllers.js
@@ -0,0 +1,5 @@
+const productsController = {
+    index: function(req, res){
+        return res.render(title);
+    },
+}
---
+++ routes/products.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+
+const productsController = require('../controllers/productsControllers');
+router.get('/', productsController.index)
