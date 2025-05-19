b8a45df9efb5007ff60ef3ac0e23e5bc555ff01d
marcosVidou <mvidou@udesa.edu.ar>
2025-05-19 16:28:03 -0300

    register y login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,31 @@
 const data = require('../db/data');
+const bcrypt = require('bcryptjs');
+const db = require('../db/models'); 
+
 const usersController = {
     login: function(req, res){
         return res.render('login');
+    },
+    processLogin: function (req, res) {
+        let userInDB = db.Usuario.findOne({
+            where: { email: req.body.email }
+        });
+        if (!userInDB) {
+            return res.render('login', {
+                error: "El email no está registrado."
+            });
+        }
+        let passCheck = bcrypt.compareSync(req.body.password, userInDB.password);
+        if (!passCheck) {
+            return res.render('login', {
+                error: "La contraseña es incorrecta."
+            });
+        }
+        req.session.userLogueado = userInDB;
+        if (req.body.remember) {
+            res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 });
+        }
+        return res.redirect('/users/profile');
     },
     profile: function(req, res){
         let productos = data.productos;
@@ -14,6 +38,27 @@ const usersController = {
     },
     register: function(req, res){
         return res.render('register');
+    },
+    processRegister: function(req, res) {
+        if (password.length < 3) {
+            return res.render('register', { error: "la contraseña debe tener al menos 3 caracteres." });
+        }
+        let userInDB = db.Usuario.findOne({ where: { email: req.body.email } });
+        if (userInDB) {
+            return res.render('register', { error: "El email ya está registrado." });
+        }
+        const passEncriptada = bcrypt.hashSync(req.body.password, 10);
+        db.Usuario.create({
+            usuario: req.body.Usuario,
+            email: req.body.email,
+            password: passEncriptada,
+            fechaNacimiento: req.body.Date,
+        })
+    },
+    logout: function(req,res){
+        req.session.destroy();
+        res.clearCookie('usuario')
+        res.redirect("/")
     }
 };
 
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,8 @@ const usersController = require('../controllers/usersController');
 router.get('/login', usersController.login);
 router.get('/profile', usersController.profile);
 router.get('/register', usersController.register);
+router.post('/register', usersController.processRegister);
+router.get('/login', usersController.login);
+router.post('/login', usersController.processLogin);
 
 module.exports = router;
